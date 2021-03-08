2d35eb774903c1eef6606d8e6f9b8905f2113a77
MK-Khan123 <mehnazkhan231@gmail.com>
2021-03-08 15:14:32 +0600

    for netlify

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -8,7 +8,7 @@ import './Shop.css';
 
 const Shop = () => {
     const first10 = fakeData.slice(0, 10);
-    const [products, setProducts] = useState(first10);
+    const [products] = useState(first10);
     const [cart, setCart] = useState([]);
 
     //Same like Review.js
